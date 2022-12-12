6ba95d293d71b7876aaecfc1265b35502fb1f828
AyrtonPereira <ayrton_pereira@live.com>
2022-12-12 12:20:55 -0300

    imp: footer finished

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,27 @@
 <script setup>
 </script>
 <template>
-  <div class="flex flex-col justify-around bg-white h-10 ">
-    <div class="flex flex-col items-center justify-around w-full">
-      <p class="text-gray-700">Seminário Profético: Todos podem profetizar - Todos os direitos reservados.</p>
-      <div class="flex items-center justify-around text-gray-600 pt-3">
-        <span class="pr-4">Facebook</span>
-        <span>Instagram</span>
+  <div class="flex flex-col justify-around bg-white h-10">
+    <div class="flex flex-col text-center items-center justify-around w-full px-2">
+      <p class="text-gray-700">
+        Seminário Profético: Todos podem profetizar - Todos os direitos
+        reservados.
+      </p>
+      <div class="flex items-center justify-evenly text-gray-600 pt-3">
+        <a href="https://m.facebook.com/profile.php?id=100000763495394" target="_blank">
+          <img class="w-8 h-8 md:w-10 md:h-10 mr-3" src="@/assets/images/facebook.svg" />
+        </a>
+        <a href="https://www.instagram.com/ezequielmachado_oficial/" target="_blank">
+          <img class="w-8 h-8 md:w-10 md:h-10" src="@/assets/images/instagram.svg" />
+        </a>
       </div>
     </div>
     <div class="flex items-center justify-center w-full py-4">
-      <p class="text-xs text-gray-600 italic hover:text-gray-700">Desenvolvido por <a class="hover:font-bold transition-all" href="https://www.linkedin.com/in/ayrton-pereira-fernandes-416484106/" target="blank">Ayrton Pereira</a></p>
+      <p class="text-xs text-gray-600 italic hover:text-gray-700">
+        Desenvolvido por
+        <a class="hover:font-bold transition-all" href="https://www.linkedin.com/in/ayrton-pereira-fernandes-416484106/"
+          target="blank">Ayrton Pereira</a>
+      </p>
     </div>
   </div>
 </template>
